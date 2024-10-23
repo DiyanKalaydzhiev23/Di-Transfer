1c3e11483ea461af71c4f3e91db67651f88ac534
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-10-23 17:50:26 +0300

    feat: added templates for login and register

--- DiTransfer/di-transfer/vite.config.js
+++ DiTransfer/di-transfer/vite.config.js
@@ -4,8 +4,29 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',  // Include your CSS or SCSS here
+                'resources/js/app.js',
+                'resources/fonts/linearicons/style.css',
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (/\.(gif|jpe?g|png|svg)$/.test(assetInfo.name)) {
+                        return 'images/[name][extname]';  // Output images in public/images
+                    }
+
+                    if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
+                        return 'fonts/[name][extname]';  // Output fonts in public/fonts
+                    }
+
+                    return '[name][extname]';
+                },
+            },
+        },
+    },
 });
