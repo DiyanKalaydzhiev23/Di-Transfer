433cbc095319723432a35f3fdf609337d9c99256
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-10-30 16:11:55 +0200

    chore: added registration logic:

--- DiTransfer/di-transfer/resources/views/register.blade.php
+++ DiTransfer/di-transfer/resources/views/register.blade.php
@@ -6,26 +6,33 @@
 <div class="wrapper">
     <div class="inner">
         <img src="/images/image-1.png" alt="" class="image-1">
-        <form action="">
+        <form method="post">
+            @csrf
+            <h3 class="log-in-header">Create an account?</h3>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="form-holder">
                 <span class="lnr lnr-user"></span>
-                <input type="text" class="form-control" placeholder="Username">
-            </div>
-            <div class="form-holder">
-                <span class="lnr lnr-phone-handset"></span>
-                <input type="text" class="form-control" placeholder="Phone Number">
+                <input type="text" name="name" class="form-control" placeholder="Username">
             </div>
             <div class="form-holder">
                 <span class="lnr lnr-envelope"></span>
-                <input type="text" class="form-control" placeholder="Mail">
+                <input type="text" name="email" class="form-control" placeholder="Mail">
             </div>
             <div class="form-holder">
                 <span class="lnr lnr-lock"></span>
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" name="password" class="form-control" placeholder="Password">
             </div>
             <div class="form-holder">
                 <span class="lnr lnr-lock"></span>
-                <input type="password" class="form-control" placeholder="Confirm Password">
+                <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password">
             </div>
             <button>
                 <span>Register</span>
--- DiTransfer/di-transfer/routes/web.php
+++ DiTransfer/di-transfer/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +15,23 @@ Route::get('/login', function () {
 
 Route::get('/register', function () {
     return view('register');
+});
+
+
+Route::post('/register', function (Request $request) {
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8|confirmed',
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    return redirect('/upload');
 });
 
 Route::get('/upload', function () {
